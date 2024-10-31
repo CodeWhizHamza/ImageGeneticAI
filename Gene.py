41a4d5a11b2fda291d9a77e9722a407158a0a93b
Hamza <muhammadhamzam1486@gmail.com>
2024-10-31 23:33:04 +0500

    Refactor Gene.py to improve code readability and maintainability

--- Gene.py
+++ Gene.py
@@ -7,11 +7,11 @@ from typing import Protocol
 class Object(Protocol):
     max_size = 50
 
-    def mutate(self):
-        pass
+    def mutate(self): ...
 
-    def render(self, canvas):
-        pass
+    def render(self, canvas): ...
+
+    def copy(self): ...
 
 
 class Ellipse(Object):
@@ -25,23 +25,19 @@ class Ellipse(Object):
             random.randint(1, Ellipse.max_size),
             random.randint(1, Ellipse.max_size),
         )
-        self.color = self.target_image[self.y, self.x].tolist()
+        self.color = [random.randint(0, 255) for _ in range(target_image.shape[2])]
         self.rotation = random.randint(0, 360)
         self.opacity = random.uniform(0.01, 1.0)
 
     def mutate(self):
-        self.x = min(
-            max(0, self.x + random.randint(-2, 2)), self.target_image.shape[1] - 1
-        )
-        self.y = min(
-            max(0, self.y + random.randint(-2, 2)), self.target_image.shape[0] - 1
-        )
+        self.x = (self.x + random.randint(-10, 10)) % self.target_image.shape[1]
+        self.y = (self.y + random.randint(-10, 10)) % self.target_image.shape[0]
         self.size = tuple(
             min(Ellipse.max_size, max(1, s + random.randint(-5, 5))) for s in self.size
         )
         self.rotation = (self.rotation + random.randint(-360, 360)) % 360
         self.opacity = min(1.0, max(0.5, self.opacity + random.uniform(-0.05, 0.05)))
-        self.color = self.target_image[self.y, self.x].tolist()
+        self.color = [min(255, max(0, c + random.randint(-10, 10))) for c in self.color]
 
     def render(self, canvas):
         color = (
@@ -61,12 +57,26 @@ class Ellipse(Object):
             -1,
         )
 
+    def copy(self):
+        new_obj = Ellipse(self.target_image, self.mutation_rate)
+        new_obj.x = self.x
+        new_obj.y = self.y
+        new_obj.size = self.size
+        new_obj.color = self.color
+        new_obj.rotation = self.rotation
+        new_obj.opacity = self.opacity
+        return new_obj
+
+    def __str__(self):
+        return f"Ellipse(x={self.x}, y={self.y}, size={self.size}, color={self.color}, rotation={self.rotation}, opacity={self.opacity})"
+
+    def __refer__(self):
+        return self.__str__()
+
 
 class Gene:
     def __init__(self, target_image: np.ndarray, mutation_rate: float):
-        self.target_image = target_image
-        self.mutation_rate = mutation_rate
-
+        self.fitness = float("inf")
         self.object = Ellipse(target_image, mutation_rate)
 
     def mutate(self):
@@ -74,3 +84,9 @@ class Gene:
 
     def render(self, canvas):
         self.object.render(canvas)
+
+    def __str__(self):
+        return f"Gene({self.object})"
+
+    def __refer__(self):
+        return self.__str__()
--- mutation_by_object.py
+++ mutation_by_object.py
@@ -1,24 +1,25 @@
 import numpy as np
 import cv2
-import random
 from Gene import Gene
+from matplotlib import pyplot as plt
 
 # Load target image
-target_image_path = "target_image_eren.jpg"
+target_image_path = "cbpunk.jpg"
 target_image = cv2.imread(target_image_path)
-height, width, _ = target_image.shape
+org_height, org_width, _ = target_image.shape
 
-target_image = cv2.resize(target_image, (width // 4, height // 4))
-height, width, _ = target_image.shape
+MAX_WIDTH = 400
+aspect_ratio = org_width / org_height
+width = min(org_width, MAX_WIDTH)
+weight = int(width / aspect_ratio)
 
-# Parameters
-generations = 10000
-mutation_rate = 0.1
+target_image = cv2.resize(target_image, (width, weight))
 
 cv2.imshow("Target Image", target_image)
 cv2.waitKey(0)
 
-# Canvas should be initialized with the average color of the target image
+print("Starting...")
+
 average_color = cv2.mean(target_image)[:3]
 canvas = np.full_like(target_image, average_color, dtype=np.uint8)
 
@@ -30,55 +31,85 @@ best_fitness = float("inf")  # Start with a very high fitness
 
 def fitness(objects):
     # Create a blank canvas
-    canvas = np.zeros_like(target_image, np.uint8)
+    canvas = np.full_like(target_image, average_color, dtype=np.uint8)
 
     # Render each object
     for obj in objects:
         obj.render(canvas)
 
+    difference = cv2.absdiff(target_image, canvas)
+
+    # print(f"Fitness: {np.sum(difference)}")
+    # plot the chart of the difference
+    # plt.imshow(cv2.cvtColor(difference, cv2.COLOR_BGR2RGB))
+    # plt.show()
+
+    # cv2.imshow("Canvas", canvas)
+    # cv2.imshow("Difference", difference)
+    # cv2.waitKey(1)
+
     # Calculate the fitness as the color difference between the target and generated images
-    diff = np.sum(np.abs(target_image - canvas))
+    diff = np.sum(difference)
     return diff
 
 
-# Evolutionary loop
+# Parameters
+generations = 5
+mutation_rate = 0.1
+REQUIRED_OBJECTS = 5000
+
+objects = [Gene(target_image, mutation_rate) for _ in range(REQUIRED_OBJECTS)]
+
+# fitness(objects)
+
+# exit()
+
 for gen in range(generations):
-    # Add a new object
-    new_object = Gene(target_image, mutation_rate)
-    objects.append(new_object)
+    """
+    1. Create 10 new genes by mutating each of the existing genes
+    2. Calculate the fitness of each gene
+    3. Select the top 100 genes based on fitness
+    4. Repeat
+    """
 
-    improved = False  # Track if we find an improvement
+    print(f"Generation {gen}")
+    canvas = np.full_like(target_image, average_color, dtype=np.uint8)
+    for obj in objects:
+        obj.object.render(canvas)
 
-    # Try mutations on the new object
-    for _ in range(100):  # Limit mutations to avoid infinite loops
-        # Mutate the object and calculate fitness
-        new_object.mutate()
-        new_fitness = fitness(objects)
+    cv2.imshow("Top Genes", canvas)
+    cv2.waitKey(1)
 
-        # Check if fitness improved
-        if new_fitness < best_fitness:
-            best_fitness = new_fitness
-            improved = True
-            break  # Keep this mutation if it improved fitness
+    # 1.
+    new_objects = []
+    for obj in objects:
+        print(obj)
+        new_objects.append(obj)
+        for _ in range(10):
+            new_obj = Gene(target_image, mutation_rate)
+            new_obj.object = obj.object.copy()
+            new_obj.mutate()
+            new_objects.append(new_obj)
 
-    if not improved:
-        # If no improvement, remove the latest object
-        objects.pop()
+    # 2.
+    for obj in new_objects:
+        obj.fitness = fitness([obj.object])
 
-    # Display progress
-    if gen % 10 == 0:
-        print(f"Generation {gen}, Fitness {best_fitness}")
-        # Show the current best rendering
-        # canvas = np.zeros_like(target_image, np.uint8)
-        # Canvas should be initialized with the average color of the target image
-        canvas = np.full_like(
-            target_image, np.mean(target_image, axis=(0, 1), dtype=np.uint8)
-        )
+    # 3.
+    new_objects.sort(key=lambda x: x.fitness)
+    objects = new_objects[:REQUIRED_OBJECTS]
 
-        for obj in objects:
-            obj.render(canvas)
-        cv2.imshow("Generated Image", canvas)
-        cv2.waitKey(1)
+    # Display the top genes
+
+    # Check if the best gene has improved
+    if objects[0].fitness < best_fitness:
+        best_fitness = objects[0].fitness
+        print(f"Best Fitness: {best_fitness}")
+
+
+print("Done!")
+print("Objects:", objects)
+
 
 # Display the final result
 canvas = np.full_like(target_image, average_color, dtype=np.uint8)
